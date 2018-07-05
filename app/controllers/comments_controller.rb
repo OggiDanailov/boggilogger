adf1ed1dcd48bffb845777accb058bbae6935356
OggiDanailov <ogidan@abv.bg>
2018-07-05 18:00:50 -0400

    deleting of comment finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,7 +20,7 @@ class CommentsController < ApplicationController
   end
 
   def  destroy
-    comment = Comment.find(params[:id])
+    comment = Comment.find(params[:comment_id])
     comment.destroy
       redirect_to '/'
 
